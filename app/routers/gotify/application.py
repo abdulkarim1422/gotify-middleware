cfa8cba0aeb1b76405d72da9e8a60b8dc905993b
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-27 11:31:25 +0300

    Remove unused HTTPException and status imports from application router

--- app/routers/gotify/application.py
+++ app/routers/gotify/application.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 import httpx
 from app.initializers import env_variables
 from app.services.gotify_auth import gotify_auth
