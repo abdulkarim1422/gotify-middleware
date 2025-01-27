d55b3738eadba16e3f246deda549e95b3524032d
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-27 13:06:31 +0300

    Refactor client creation endpoint to use query headers for authentication

--- app/routers/gotify/client.py
+++ app/routers/gotify/client.py
@@ -15,12 +15,19 @@ async def get_clients(query_header: tuple = Depends(gotify_auth)):
         return response.json()
 
 @router.post("/client", summary="Create a client")
-async def create_client(api_key: str = Depends(gotify_auth)):
-    async with httpx.AsyncClient() as client:
-        req = client.build_request("POST", f"{env_variables.GOTIFY_URL}/client?token={api_key}", json=client.dict())
-        response = await client.send(req)
+async def create_client(request_body: dict, query_header: tuple = Depends(gotify_auth)):
+    async with httpx.AsyncClient() as async_client:
+        query, header = query_header
+        req = async_client.build_request(
+            "POST",
+            f"{env_variables.GOTIFY_URL}/client?token={query}",
+            headers={"Authorization": header},
+            json=request_body
+        )
+        response = await async_client.send(req)
         return response.json()
-    
+
+
 @router.put("/client/{id}", summary="Update a client")
 async def update_client(id: int, client: dict, api_key: str = Depends(gotify_auth)):
     async with httpx.AsyncClient() as client:
