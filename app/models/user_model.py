324bd7e65a067510ea4bc9cef9fb7292163ff6ed
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-12 14:49:47 +0300

    Add User model to define user data structure for the application

---
+++ app/models/user_model.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import datetime
+import uuid
+
+# database class to store user data (gotify side)
+class User(SQLModel, table=True):
+    id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
+    username: str
+    password: str
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=datetime.now())
+    client_token: str
+    full_name: str
