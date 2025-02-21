e7614607b3314d8225369fb3f4616ab484c2feab
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 15:05:09 +0300

    Refactor get_users_activity endpoint to use Pydantic model for user list input

--- app/routers/internal/activity.py
+++ app/routers/internal/activity.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, Request, Query
+from fastapi import APIRouter, Request
 from app.services import activity_service
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class UsersList(BaseModel):
+    list_of_users: list
 
 @router.get("/activity")
 async def get_users_activity(
     request: Request,
-    list_of_users: list = Query(None, description="List of users") # list of usernames
+    users_list: UsersList
 ):
     users_activity = []
-    for username in list_of_users:
+    for username in users_list.list_of_users:
         user_last_activity = activity_service.get_user_last_active(username)
         users_activity.append({"username": username, "last_active": user_last_activity})
     return users_activity
