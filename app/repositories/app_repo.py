96fbb5063d5dd2a0f036e5bfa17b13ee725aeb48
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 15:47:08 +0300

    Refactor app repository to use app_model for App model references

--- app/repositories/app_repo.py
+++ app/repositories/app_repo.py
@@ -1,25 +1,25 @@
 from app.initializers import db
-from app import models
+from app.models import app_model
 from sqlmodel import select
 
 def get_all_apps():
     with db.get_session() as session:
-        apps = session.exec(select(models.App)).all()
+        apps = session.exec(select(app_model.App)).all()
         return apps
     
 def get_app_by_id(app_id):
     with db.get_session() as session:
-        app = session.get(models.App, app_id)
+        app = session.get(app_model.App, app_id)
         return app
     
 def get_apps_by_user_id(user_id):
     with db.get_session() as session:
-        apps = session.exec(select(models.App).where(models.App.user_id == user_id)).all()
+        apps = session.exec(select(app_model.App).where(app_model.App.user_id == user_id)).all()
         return apps
     
 def get_apps_by_project_id(project_id):
     with db.get_session() as session:
-        apps = session.exec(select(models.App).where(models.App.project_id == project_id)).all()
+        apps = session.exec(select(app_model.App).where(app_model.App.project_id == project_id)).all()
         return apps
     
 def create_app(app):
