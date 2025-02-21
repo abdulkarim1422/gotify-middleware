a44adcd94b3a3aa39e00cc122e614eeea4d6b06d
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 14:36:52 +0300

    Add update_user_last_active function to track user activity

---
+++ app/services/last_active.py
@@ -0,0 +1,14 @@
+from app.services.gotify.user import current_user_via_token
+from app.repositories import user_repo
+from datetime import datetime
+
+def update_user_last_active(x_gotify_key):
+    # get user vie x-gotify-key
+    user_json = current_user_via_token(x_gotify_key)
+    
+    # get user from db
+    user_obj = user_repo.get_user_by_username(user_json["name"])
+
+    # update user last active
+    user_obj.last_active = datetime.now()
+    user_repo.update_user(user_obj)
