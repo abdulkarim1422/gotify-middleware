70f9a979d397358259c068b29dc63623f0407bda
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 15:47:11 +0300

    Refactor user repository to use user_model for User references

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -1,20 +1,20 @@
 from app.initializers import db
-from app import models
+from app.models import user_model
 from sqlmodel import select
 
 def get_all_users():
     with db.get_session() as session:
-        users = session.exec(select(models.User)).all()
+        users = session.exec(select(user_model.User)).all()
         return users
 
 def get_user_by_id(user_id):
     with db.get_session() as session:
-        user = session.get(models.User, user_id)
+        user = session.get(user_model.User, user_id)
         return user
     
 def get_user_by_username(username):
     with db.get_session() as session:
-        user = session.exec(select(models.User).where(models.User.username == username)).first()
+        user = session.exec(select(user_model.User).where(user_model.User.username == username)).first()
         return user
     
 def create_user(user):
