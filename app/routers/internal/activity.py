b3af861bb592310c56453d174f4a0f7473c930c9
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 19:41:59 +0300

    Remove deprecated get_users_activity endpoint from activity router

--- app/routers/internal/activity.py
+++ app/routers/internal/activity.py
@@ -7,17 +7,6 @@ router = APIRouter()
 class UsersList(BaseModel):
     list_of_usernames: list
 
-@router.post("/activitise")
-async def get_users_activity(
-    request: Request,
-    users_list: UsersList
-):
-    users_activity = []
-    for username in users_list.list_of_usernames:
-        user_last_activity = activity_service.get_user_last_active(username)
-        users_activity.append({"username": username, "last_active": user_last_activity})
-    return users_activity
-
 @router.post("/activity")
 async def get_user_activity(
     request: Request,
