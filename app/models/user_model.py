d5905a9bb287b24e090acd7d182c2d532bb6e556
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-02-21 18:30:40 +0300

    Make full_name optional in User model and remove tablename attribute

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -5,13 +5,11 @@ import uuid
 
 # database class to store user data (gotify side)
 class User(SQLModel, table=True):
-    __tablename__ = "users"
-
     id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
     username: str
     password: str
     created_at: datetime = Field(default=datetime.now())
     updated_at: datetime = Field(default=datetime.now())
     client_token: str
-    full_name: str
+    full_name: Optional[str] = None
     last_active: datetime = Field(default=datetime.now())
